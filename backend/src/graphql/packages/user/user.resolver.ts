e23ac352d400a5c650d7ec716fa395ec2233ab70
sl-chern <slavikchernog@gmail.com>
2024-07-24 12:37:32 +0300

    fixed e2e tests

--- backend/src/graphql/packages/user/user.resolver.ts
+++ backend/src/graphql/packages/user/user.resolver.ts
@@ -1,13 +1,45 @@
-import { Resolver, Query, Args } from '@nestjs/graphql';
+import { Resolver, Query, Args, ObjectType, Field, Mutation } from '@nestjs/graphql';
 import { User } from '../../../graphql/types/user.model';
 import { UserService } from './user.service';
+import { BucketService } from 'src/bucket/bucket.service';
 
+@ObjectType()
+class UrlReturn {
+  @Field(type => String)
+  url: string;
+}
 @Resolver(of => User)
 export class UserResolver {
-  constructor(private readonly userService: UserService) {}
+  constructor(
+    private readonly userService: UserService,
+    private readonly bucketService: BucketService,
+  ) {}
 
   @Query(returns => User)
   async getUserById(@Args('userId') id: string): Promise<User> {
     return this.userService.findById(id);
+  }
+
+  @Query(returns => UrlReturn)
+  async getImageUrl(): Promise<UrlReturn> {
+    return { url: await this.bucketService.generateSignedURL('FxmJJ9TXwAAZ_5u.png') };
+  }
+
+  @Mutation(returns => UrlReturn)
+  async uploadImage(
+    @Args('imageName') imageName: string,
+    @Args('image') image: string,
+  ): Promise<UrlReturn> {
+    return { url: await this.bucketService.uploadFile(imageName, image) };
+  }
+
+  @Query(returns => Boolean)
+  async deleteImage(@Args('imageName') imageName: string): Promise<boolean> {
+    try {
+      this.bucketService.deleteFile(imageName);
+      return true;
+    } catch {
+      return false;
+    }
   }
 }
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,6 +4,7 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { ValidationPipe } from '@nestjs/common';
 import { HttpExceptionFilter } from './graphql/filters/http.exeption.filter';
 import * as cookieParser from 'cookie-parser';
+import { json } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -15,6 +16,7 @@ async function bootstrap() {
   app.use(cookieParser());
   app.enableCors({ origin: 'http://localhost:3000', credentials: true });
   app.setGlobalPrefix('api/v1');
+  app.use(json({ limit: '50mb' }));
   app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(process.env.BACKEND_PORT);
 }
